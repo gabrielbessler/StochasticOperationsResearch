254a0fdbb3c8514ef04f70d2a497cce0397cce44
Gabriel <gabrielbessler@gmail.com>
2020-05-05 06:51:33 -0500

    Modified algorithm to handle imperfect games

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import copy
 import sys
 from collections import defaultdict
+import numpy as np
 sys.setrecursionlimit(100000)
 
 class Game:
@@ -30,6 +31,13 @@ class Strategy:
             return f"Outcome: {str(self.outcome)}"
 
         return f"Outcome: {str(self.outcome)}, Actions: {self.actions}"
+
+def get_info_set(game, node): 
+    for info_set in game.info_sets: 
+        if node.node_id in info_set:
+            return info_set
+
+    return set([node.node_id])
 
 def get_subperf_equil(game, node, trace=False):
     '''If node is a subgame, return a dictionary of all subgame perfect equillibria.
@@ -93,25 +101,154 @@ def get_subperf_equil(game, node, trace=False):
         print(f"{node.node_id}: {all_strategies}")
 
     # It is a valid subgame - find the Nash equil.
-    max_payoff = -float('inf')
-    max_payoff_actions = []
-    for action in node.actions:
-        outcome = all_strategies[node.actions[action].node_id][0].outcome
-        payoff = game.payoffs[outcome][node.player]
-        if payoff > max_payoff:
-            max_payoff = payoff
-            max_payoff_actions = [action]
-        elif payoff == max_payoff:
-            max_payoff_actions += [action]
+    return get_nash_equil(game, node, all_strategies)
 
+def get_payoff(p1_strategy, p2_strategy, node, game, info_set_p1, info_set_p2, all_strategies): 
+    while True: 
+        # If the node has already been solved, it's already in our strategy set.
+        if node.node_id in all_strategies:
+            outcome = all_strategies[node.node_id][0].outcome
+            return game.payoffs[outcome], outcome
+
+        if node.player == 0: 
+            # Find the information set this node is part of 
+            for i, info_set in enumerate(info_set_p1.keys()):
+                if node.node_id in info_set:
+                    action_to_take = p1_strategy[i]
+        else: 
+            for i, info_set in enumerate(info_set_p2.keys()):
+                if node.node_id in info_set:
+                    action_to_take = p2_strategy[i]
+
+        node = node.actions[list(node.actions.keys())[action_to_take]]
+        if node.outcome is not None:
+            return game.payoffs[node.outcome], node.outcome
+
+def get_nash_equil(game, node, all_strategies):
+    # First, find all of the information sets for each player (here we assume num_players = 2).
+    # Note: This is slow. Pass the nodes up the same way we check for if something is a subgame?
+    info_sets_p1 = {}
+    info_sets_p2 = {}
+
+    num_strategies_p1 = 1
+    num_strategies_p2 = 1
+    
+    outcomes = {}
+    
+    unvisited = []
+    unvisited += [node]
+
+    # Figure out which information sets are relevant for this subgame, 
+    # and how many possible strategies there are.
+    while len(unvisited) != 0: 
+        curr_node = unvisited[-1]
+        unvisited = unvisited[:-1]
+
+        # If the node has already been solved, it should not count. 
+        if curr_node.node_id in all_strategies:
+            continue
+
+        for action in curr_node.actions:
+            unvisited += [curr_node.actions[action]]
+        
+        info_set = tuple(get_info_set(game, curr_node))
+        if curr_node.player == 0 and info_set not in info_sets_p1.keys():
+            if len(curr_node.actions) != 0:
+                info_sets_p1[info_set] = len(curr_node.actions)
+                num_strategies_p1 *= len(curr_node.actions)
+        elif curr_node.player == 1 and info_set not in info_sets_p2.keys():
+            if len(curr_node.actions) != 0:
+                info_sets_p2[info_set] = len(curr_node.actions)
+                num_strategies_p2 *= len(curr_node.actions)
+
+    p1_strategy = [0] * len(info_sets_p1.keys())
+    p2_strategy = [0] * len(info_sets_p2.keys())
+
+    payoff_matrix = np.zeros((num_strategies_p1, num_strategies_p2, 2))
+    payoff_matrix_strategies = {} 
+    for i in range(num_strategies_p1):
+        for j in range(num_strategies_p2):
+            payoff, _ = get_payoff(p1_strategy, p2_strategy, node, game, info_sets_p1, info_sets_p2, all_strategies)
+            payoff_matrix[i, j] = payoff
+            payoff_matrix_strategies[(i, j)] = (tuple(p1_strategy), tuple(p2_strategy))
+
+            for p, key in enumerate(info_sets_p2.keys()):
+                if p2_strategy[p] < info_sets_p2[key] - 1:
+                    p2_strategy[p] += 1
+                    break
+            
+                p2_strategy[p] = 0
+
+        for p, key in enumerate(info_sets_p1.keys()):
+            if p1_strategy[p] < info_sets_p1[key] - 1:
+                p1_strategy[p] += 1
+                break
+            
+    # For each row, pick p2's best responses.
+    all_best_responses = []
+    for i in range(num_strategies_p1):
+        best_responses = []
+        best_response_val = -float('inf')
+        for j in range(num_strategies_p2): 
+            if payoff_matrix[i, j, 1] == best_response_val:
+                best_responses += [(i, j)]
+            elif payoff_matrix[i, j, 1] > best_response_val:
+                best_response_val = payoff_matrix[i, j, 1]
+                best_responses = [(i, j)]
+        all_best_responses += best_responses
+
+    # For each col, pick p1's best response.
+    all_best_responses_2 = []
+    for j in range(num_strategies_p2):
+        best_responses = []
+        best_response_val = -float('inf')
+        for i in range(num_strategies_p1): 
+            if payoff_matrix[i, j, 0] == best_response_val:
+                best_responses += [(i, j)]
+            elif payoff_matrix[i, j, 0] > best_response_val:
+                best_response_val = payoff_matrix[i, j, 0]
+                best_responses = [(i, j)]
+        all_best_responses_2 += best_responses
+
+    # Get the strategies for each index in the payoff matrix.
+    equils = set(all_best_responses).intersection(set(all_best_responses_2))
+    strats = []
+    for e in equils:
+        strats += [payoff_matrix_strategies[e]]
+
+    # Make the new strategies
     new_strategies = defaultdict(list)
-    for action in max_payoff_actions: 
-        for strategy in all_strategies[node.actions[action].node_id]: 
-            new_actions = copy.deepcopy(strategy.actions) # each strategy is a dictionary, so copy it
-            new_actions[node.node_id] = action
-            new_strategies[node.node_id] += [Strategy(strategy.outcome, new_actions)]
+    for strat in strats:
+        p_0_moves = strat[0]
+        p_1_moves = strat[1]
+        actions = {}
+        for i in range(len(p_0_moves)):
+            info_set = list(info_sets_p1.keys())[i]
+            for curr_node in info_set:
+                actions[curr_node] = p_0_moves[i]
+
+        for i in range(len(p_1_moves)):
+            info_set = list(info_sets_p2.keys())[i]
+            for curr_node in info_set:
+                actions[curr_node] = p_1_moves[i]
+
+        new_strat = Strategy(get_payoff(p_0_moves, p_1_moves, node, game, info_sets_p1, info_sets_p2, all_strategies)[1], actions)
+        all_substrategies = update(new_strat, all_strategies, node)
+        for substrategy in all_substrategies:  
+            subactions = copy.deepcopy(substrategy.actions)
+            new_strat.actions.update(subactions)
+            new_strategies[node.node_id] += [new_strat]
 
     return new_strategies, None
+
+def update(strat, all_strategies, node):
+    while True: 
+        if node.node_id in all_strategies:
+            return all_strategies[node.node_id]
+        
+        action_idx = strat.actions[node.node_id]
+        node = node.actions[list(node.actions.keys())[action_idx]]
+    return strat
 
 def generate_centipede_nodes(n, k=2, k2=None):
     info_sets = []
@@ -177,36 +314,44 @@ def generate_centipede_nodes(n, k=2, k2=None):
     return payoffs, nodes[-1], info_sets
 
 def generate_incomplete_game():
+    # Leaf nodes
+    node_15  = Node(15, {}, 'H', 0)
+    node_14  = Node(14, {}, 'G', 0)
+    node_13  = Node(13, {}, 'F', 0)
+    node_12  = Node(12, {}, 'E', 0)
+    node_11  = Node(11, {}, 'D', 0)
+    node_10  = Node(10, {}, 'C', 0)
+    node_9   = Node(9,  {}, 'B', 0)
+    node_8   = Node(8,  {}, 'A', 0)
+
+    node_7   = Node(7, {'m': node_14, 'n': node_15}, None, 0)
+    node_6   = Node(6, {'k': node_12, 'l': node_13}, None, 0)
+    node_5   = Node(5, {'i': node_10, 'j': node_11}, None, 0)
+    node_4   = Node(4, {'g': node_8,  'h': node_9},  None, 0)
+
+    node_3   = Node(3, {'e': node_6, 'f': node_7}, None, 1)
+    node_2   = Node(2, {'c': node_4, 'd': node_5}, None, 1)
+
+    node_1   = Node(1, {'a': node_2, 'b': node_3}, None, 0)
+
+    payoffs  = {'A': [2, 0], 'B': [0, 1], 'C': [2, 0], 'D': [0, 0], 
+                'E': [0, 0], 'F': [1, 0], 'G': [0, 0], 'H': [1, 0]}
     
-    node_six  = Node(6, {}, 'D', 0) # leaf
-    node_five  = Node(5, {}, 'C', 0) # leaf
-    node_four  = Node(4, {}, 'B', 0) # leaf
-    node_three = Node(3, {}, 'A', 0) # leaf 
-
-    node_two   = Node(2, {'c': node_five,  'd': node_six}, None, 0)
-    node_one   = Node(1, {'e': node_three, 'f': node_four}, None, 0)
-
-    root_node  = Node(0, {'a': node_one, 'b': node_two}, None, 1)
-
-    payoffs = {'A': [0, 0], 'B': [0, 0], 'C': [0, 0], 'D': [0, 0]}
-    
-    info_sets = [set([1, 2])]
-    # info_sets = [set([1]), set([2])]
-    return payoffs, root_node, info_sets
+    info_sets = [set([2, 3])]
+    return payoffs, node_1, info_sets
 
 def main():
-    # payoffs, root, info_sets = generate_centipede_nodes(10, 10, 0)
+    # payoffs, root, info_sets = generate_centipede_nodes(10, 8, 0)
     # num_players = 2
     # game = Game(root, payoffs, num_players, info_sets)
     # res, subtree = get_subperf_equil(game, game.root)
     # print(res)
 
     payoffs, root, info_sets = generate_incomplete_game()
-    num_players = 1
+    num_players = 2
     game = Game(root, payoffs, num_players, info_sets)
     res, subtree = get_subperf_equil(game, game.root, True)
     print(res, subtree)
 
 if __name__ == "__main__":
     main()
-
